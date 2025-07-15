7d2d3144acc6c02be6471301f905485c9ad89357
Miller <millermarru4@gmail.com>
2025-07-14 22:36:43 -0500

    register

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,16 @@
+function PageRegister() {
+  
+    return (
+
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="w-full max-w-md">
+        <h1 className="text-2xl font-bold">Register</h1>
+        <p className="mt-2 text-gray-600">Create a new account to get started.</p>
+        {/* Registration form will go here */}
+      </div>
+    </div>
+  );
+
+}
+
+export default PageRegister;
